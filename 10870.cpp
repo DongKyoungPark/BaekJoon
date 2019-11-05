ebf80f8bbb42a08ae1aa524076f99def58eb47a0
DongKyoungPark <oe1lolo@nate.com>
2019-11-05 11:59:18 +0900

    백준 정답 소스코드

---
+++ 10870.cpp
@@ -0,0 +1,24 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int fibonachi(int _n)
+//{
+//	if (_n <= 1)
+//		return _n;
+//
+//	return fibonachi(_n - 1) + fibonachi(_n - 2);
+//}
+//
+//int main(void)
+//{
+//	cin.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//
+//	int n;
+//	
+//	cin >> n;
+//	cout << fibonachi(n);
+//
+//	return 0;
+//}
---
+++ 3053.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#define PI 3.14159265358979323846
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	cin.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//
+//	long double R;
+//
+//	cin >> R;
+//	cout << fixed;
+//	cout.precision(6);
+//
+//	cout << R * R * PI << '\n';
+//	cout << 2.0 * R * R << '\n';
+//
+//	return 0;
+//}

a5658743ff316434232c6d337b64b8534e244b76
DongKyoungPark <oe1lolo@nate.com>
2019-11-11 16:33:06 +0900

    백준 정답 소스코드

---
+++ 1427.cpp
@@ -0,0 +1,21 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//
+//using namespace std;
+//
+//int main(void)
+//{
+//	cin.tie(NULL);
+//	ios_base::sync_with_stdio(false);
+//
+//	string s;
+//	cin >> s;
+//
+//	sort(s.begin(), s.end());
+//	reverse(s.begin(), s.end());
+//
+//	cout << s;	
+//
+//	return 0;
+//}
